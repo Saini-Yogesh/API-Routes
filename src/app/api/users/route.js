2e68bf68651b8bcd6fcc7794554ab97cb23b466e
Yogesh Saini <yogesh.saini4002@gmail.com>
2024-10-08 13:31:44 +0530

    Make API with Post Method

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,8 +1,25 @@
 import { user } from "@/util/db";
-
 const { NextResponse } = require("next/server");
 
 export function GET() {
   const data = user;
   return NextResponse.json(data, { status: 200 });
+}
+
+export async function POST(request) {
+  let payload = await request.json();
+  console.log(payload.name);
+  if (!payload.name || !payload.age || !payload.mail) {
+    return NextResponse.json(
+      { result: "required field not found", success: false },
+      { status: 400 }
+    );
+  }
+  return NextResponse.json(
+    {
+      result: "Successfully user created",
+      success: true,
+    },
+    { status: 201 }
+  );
 }
