a541b44519ba48c0582b79d6376a09f8fdace278
Yogesh Saini <yogesh.saini4002@gmail.com>
2024-10-08 19:01:14 +0530

    Make DELETE API

--- src/app/api/users/[userId]/route.js
+++ src/app/api/users/[userId]/route.js
@@ -24,3 +24,18 @@ export async function PUT(request, content) {
   }
   return NextResponse.json({ result: payload, success: true }, { status: 200 });
 }
+
+export function DELETE(request, content) {
+  let id = content.params.userId;
+  if (id) {
+    return NextResponse.json(
+      { result: "User Deleted", success: true },
+      { status: 200 }
+    );
+  } else {
+    return NextResponse.json(
+      { result: "internal error", success: false },
+      { status: 400 }
+    );
+  }
+}
